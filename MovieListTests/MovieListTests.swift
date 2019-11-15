1141ee9ba41e483be339ca967c1ff6b712c4fb0c
rugmangathan <rmk>
2019-11-15 09:34:34 +0530

    Initial Commit

---
+++ MovieListTests/MovieListTests.swift
@@ -0,0 +1,34 @@
+//
+//  MovieListTests.swift
+//  MovieListTests
+//
+//  Created by Captain America on 15/11/19.
+//  Copyright © 2019 rugmangathan. All rights reserved.
+//
+
+import XCTest
+@testable import MovieList
+
+class MovieListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
